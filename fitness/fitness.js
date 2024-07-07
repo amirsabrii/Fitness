a390991eafa595d8478e9c5541841705e0fa1a17
amir <amryamiir@gmail.com>
2024-07-07 15:40:49 +0330

    fitness

---
+++ fitness/fitness.js
@@ -0,0 +1,9 @@
+function openNav() {
+    document.querySelector(".navbar-item > ul").style.display = "block";
+    document.querySelector(".fa-times").style.display = "block";
+    }
+
+    function closeNav() {
+        document.querySelector(".navbar-item > ul").style.display = "none";
+        }
+        
