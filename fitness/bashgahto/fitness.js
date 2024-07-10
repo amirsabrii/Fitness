b9e50178e396641776e8b1fe913fc0ef30ab0bb0
amir <amryamiir@gmail.com>
2024-07-10 23:34:33 +0330

    fitness add item

---
+++ fitness/bashgahto/fitness.js
@@ -0,0 +1,19 @@
+function openNav() {
+    document.querySelector(".navbar-item > ul").style.display = "block";
+    document.querySelector(".fa-times").style.display = "block";
+    }
+
+    function closeNav() {
+        document.querySelector(".navbar-item > ul").style.display = "none";
+        }
+        
+let openMenu = document.querySelector("#open-menu")
+let menu = document.querySelector(".said-menu li > ul")
+
+openMenu.addEventListener(("mouseover") ,e => {
+    menu.style.display = "block"
+})
+
+openMenu.addEventListener(("mouseout") ,e => {
+    menu.style.display = "none"
+})
--- fitness/fitness.js
+++
@@ -1,9 +0,0 @@
-function openNav() {
-    document.querySelector(".navbar-item > ul").style.display = "block";
-    document.querySelector(".fa-times").style.display = "block";
-    }
-
-    function closeNav() {
-        document.querySelector(".navbar-item > ul").style.display = "none";
-        }
-        
